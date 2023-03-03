9c633f23fe88f4c19f4063b5c0e3a2a0ec03702e
sadhanajarag <sadhana20.jarag@gmail.com>
2023-03-04 01:03:25 +0530

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request,jsonify
 from googleapiclient.discovery import build
+from flask_cors import CORS,cross_origin
 import pandas as pd
+import pymongo
+
 app = Flask(__name__)
 
 @app.route('/', methods=("POST", "GET"))
+@cross_origin()
 def yt_scrapping():
     api_key = 'AIzaSyCyDyT32dJHEkLcdMhsZ0k0TAlC-VHPc-U'
     channel_id = 'UCphU2bAGmw304CFAzy0Enuw'
@@ -39,9 +43,16 @@ def yt_scrapping():
     Video_data = pd.DataFrame(Video_details)
     Video_data = Video_data.set_index([pd.Index([1,2,3,4,5])])
     #Video_data = Video_data.reset_index()
+    client = pymongo.MongoClient("mongodb+srv://sadhana20jarag:1234@cluster0.udydp5r.mongodb.net/?retryWrites=true&w=majority")
+    db = client['ytscrap_new']
+    coll_pw_eng = db['yt_scrap_pwd_new']
+    coll_pw_eng.insert_many(Video_details)
     Video_data.to_csv('Video_Details_PW_Foundation.csv')
+    #Video_data.to_csv('Video_Details_PW_Foundation.csv')
     data_final=pd.read_csv('Video_Details_PW_Foundation.csv',index_col=0)
+    records_ = data_final.to_dict(orient = 'records') 
+    result = db.coll_pw_eng.insert_many(records_ ) 
     return render_template('result.html', tables=[data_final.to_html()], titles=[''])
                     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0',port=8000)
